ec73ac3438cf6aa334f7a7b586ed4866300886e5
fulluhq <268378461+fulluhq@users.noreply.github.com>
2026-03-16 17:38:15 +0100

    Delete CsStealer_v22 directory

--- CsStealer_v22/auth/Credits.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace CsStealer
-{
-    class Credit
-    {
-        public static string Header = "[ CS Stealer by Noone ]\n\n-> https://guns.lol/noone.cs\n-> https://t.me/cslogs\n\n";
-    }
-}
--- CsStealer_v22/auth/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace CsStealer
-{
-    public class Program
-    {
-        async static Task Main(string[] Args) => await __Main__.Start(Args);
-    }
-}
